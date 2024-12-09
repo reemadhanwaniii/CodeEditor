4a08acdc8ec2f99aefad61002c1d6beb71d9f088
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-09 19:59:15 +0530

    integrated sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Navbar from './components/Navbar';
+import SideBar from './components/Sidebar';
 import Description from './pages/Description/Description'
 import React from 'react';
 
@@ -16,6 +17,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <SideBar/>
       <Description descriptionText={markdownText}/>
     </>
   )
